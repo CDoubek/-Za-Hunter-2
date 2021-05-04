ba8eac75c46f088311c82c560b77da0ea6fc5df7
Colin Doubek <doubekcolin@gmail.org>
2021-05-04 10:36:10 -0500

    Initial Commit

---
+++ 'Za Hunter 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  'Za Hunter 2
+//
+//  Created by Student on 5/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 'Za Hunter 2/_Za_Hunter_2App.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_Hunter_2App.swift
+//  'Za Hunter 2
+//
+//  Created by Student on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct _Za_Hunter_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
